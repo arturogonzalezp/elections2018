c7d4f6fd3db1eadb23a54837c0c170c276cf9817
César Arturo González <arthurgonzalezp@gmail.com>
2018-01-23 16:37:15 -0600

    Interface base finished

--- elections/utils/streamer.py
+++ elections/utils/streamer.py
@@ -1,6 +1,43 @@
 import tweepy
 class TweetsStreamer(tweepy.StreamListener):
-    counter = 0
+    buffer = []
+    def on_connect(self):
+        """Called once connected to streaming server"""
+        print ('On Connect')
+    def keep_alive(self):
+        """Called when a keep-alive arrived"""
+        print ('Keep Alive')
     def on_status(self, status):
-        self.counter += 1
-        print(str(self.counter) + ") " + status.text)
+        """Called when a new status arrives"""
+        print (status.text)
+        self.buffer.append(status)
+    def on_exception(self, exception):
+        """Called when an unhandled exception occurs."""
+        print ('On Exception')
+    def on_delete(self, status_id, user_id):
+        """Called when a delete notice arrives for a status"""
+        print ('On Delete')
+    def on_event(self, status):
+        """Called when a new event arrives"""
+        print ('On Event')
+    def on_direct_message(self, status):
+        """Called when a new direct message arrives"""
+        print ('On Direct Message')
+    def on_friends(self, friends):
+        """Called when a friends list arrives"""
+        print ('On Friends')
+    def on_limit(self, track):
+        """Called when a limitation notice arrives"""
+        print ('On Limit')
+    def on_error(self, status_code):
+        """Called when a non-200 status code is returned"""
+        print ('On Error')
+    def on_timeout(self):
+        """Called when stream connection times out"""
+        print ('On Timeout')
+    def on_disconnect(self, notice):
+        """Called when twitter sends a disconnect notice"""
+        print ('On Disconnect')
+    def on_warning(self, notice):
+        """Called when a disconnection warning message arrives"""
+        print ('On Warning')
