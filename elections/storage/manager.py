444bf86410e23cc74ab9d15f1df147c077004bc4
César Arturo González <arthurgonzalezp@gmail.com>
2018-02-06 15:59:13 -0600

    Fixed Manager

--- elections/storage/manager.py
+++ elections/storage/manager.py
@@ -1,14 +1,23 @@
-import pymysql
-from connection import Connection
+import json
+import re
+from elections.storage.connection import Connection
 
 class Manager:
-    buffer_size = 10
-    
-    def insert_tweet(self, buffer):
+    def __init__(self,buffer_size):
+        self.buffer_size = buffer_size
+        self.buffer = []
+    def insert_tweet(self,tweet):
+        self.buffer.append(tweet)
+        if(self.buffer.__len__() == self.buffer_size):
+            self.save()
+            self.buffer = []
+
+    def save(self):
         db = Connection()
-        for tweet in buffer:
-            string = "(" + tweet.text + ")".encode('utf-8').replace('\n','')
-            db.cursor.execute("INSERT INTO mined_data (raw_tweet, tweet_id) VALUES ('" + string + "', '" + tweet.id_str + "')")
-            
+        for tweet in self.buffer:
+            tweet_str = re.escape(self.tweet_to_json(tweet))
+            db.cursor.execute("INSERT INTO MinedData (raw_tweet, tweet_id) VALUES ('" + tweet_str + "', '" + tweet.id_str + "')")
         db.cursor.close()
         db.connection.close()
+    def tweet_to_json(self,tweet):
+        return json.dumps(tweet._json)
