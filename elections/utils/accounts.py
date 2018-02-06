333dbb5f76c857d0fa0a181abe12a2d3854c48af
MikeSagnelli <ing.sagnelli@gmail.com>
2018-02-06 17:05:53 -0600

    Added users.txt in .gitIgnore, and added dynamic loading of user accounts with a .txt file.

--- elections/utils/accounts.py
+++ elections/utils/accounts.py
@@ -1,6 +1,14 @@
+import os
+
+ROOT = lambda base : os.path.join(os.path.dirname(__file__), base).replace('\\','/')
+
 class Accounts:
+
     def __init__(self):
-        self.accounts = ["j_alex747", "cesararturo94", "alfonsoirai"]
+        self.accounts = []
+        self.user_file = open(ROOT('users.txt'), "r")
+        for i, line in enumerate(self.user_file):
+            self.accounts.insert(i, line)
 
     def check_user(self, user):
         return user in self.accounts
