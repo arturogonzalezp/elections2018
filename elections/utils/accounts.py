e85239e255792844377a2b68f4f102bca5f5f90d
MikeSagnelli <ing.sagnelli@gmail.com>
2018-02-07 00:37:20 -0600

    Added closing file, good practice.

--- elections/utils/accounts.py
+++ elections/utils/accounts.py
@@ -7,8 +7,11 @@ class Accounts:
     def __init__(self):
         self.accounts = []
         self.user_file = open(ROOT('users.txt'), "r")
+        
         for i, line in enumerate(self.user_file):
             self.accounts.insert(i, line)
+
+        self.user_file.close()
 
     def check_user(self, user):
         return user in self.accounts
