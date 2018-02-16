d14a3694a0a26dcc8899e693d4923ae5c58dacb3
alfonsoirai <pon.alfonsoirai@gmail.com>
2018-02-15 23:52:53 -0600

    added local storage capability

--- elections/storage/manager.py
+++ elections/storage/manager.py
@@ -1,6 +1,7 @@
 import json
 import re
 from elections.storage.connection import Connection
+FILE_NAME = "local_storage.json"
 
 class Manager:
     def __init__(self,buffer_size):
@@ -12,9 +13,20 @@ class Manager:
             if(Connection.mode == 'PROD'):
                 self.save()
             elif(Connection.mode == 'DEV'):
-                # Modify here poncho!
-                print('Dev mode, not saved')
+                self.save_local()
+                print "saved"
             self.buffer = []
+    def save_local(self):
+        try:
+            file = open(FILE_NAME, 'w')
+        except IOError:
+            print "Error opening file: " + FILE_NAME
+        else:
+            temp_buffer = []
+            for tweet in self.buffer:
+                temp_buffer.append(tweet._json)
+            file.write(self.tweets_to_json(temp_buffer))
+            file.close()
     def save(self):
         db = Connection()
         for tweet in self.buffer:
@@ -24,3 +36,6 @@ class Manager:
         db.connection.close()
     def tweet_to_json(self,tweet):
         return json.dumps(tweet._json)
+    
+    def tweets_to_json(self,tweets):
+        return json.dumps(tweets)
